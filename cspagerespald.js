8a9567692d65b5da2e3c936727d4ff8e439e4e8b
jesuslazoquevedo <jesuslazoquevedo@gmail.com>
2017-07-23 05:22:29 -0500

    opciones

--- cspagerespald.js
+++ cspagerespald.js
@@ -16,3 +16,46 @@ function start() {
 }
 
 window.addEventListener("load", start, false);
+
+function entity(elem){
+    var entidad = document.getElementById("elem");
+    if (entidad.checked){
+        search(elem);
+    }
+    else {
+        showNothing();
+    }
+}
+
+function showNothing(){
+    var estado = document.getElementById("page1");
+    estado.style.backgroundColor= "red";
+}
+
+function search(){
+    var persona = document.getElementById("persona");
+    var proceso = document.getElementById("proceso");
+    var pregunta = document.getElementById("pregunta");
+    if (pregunta.checked){
+        show("pregunta");    
+    }
+    else if (proceso.checked){
+        show("proceso");
+    }
+    else{
+        show("persona");
+    }
+}
+
+function show(elem){
+    var process = document.getElementById("page1");
+    if (elem == "proceso"){
+        process.style.backgroundColor = "red"; 
+    }
+    else if (elem == "pregunta"){
+        process.style.backgroundColor = "green"; 
+    }
+    else {
+        process.style.backgroundColor = "yellow"; 
+    }
+}
