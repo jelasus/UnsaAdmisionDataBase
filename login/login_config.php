e1c7056fc708d4e4a47147c434ea89d0500663d2
k-st <kast46.15@gmail.com>
2017-07-23 00:11:36 -0500

    Delete login_config.php

--- login/login_config.php
+++
@@ -1,9 +0,0 @@
-<?php
-    $servidor = "localhost";
-    $usuario = "ii";
-    $password = "chalenyer";
-    $bd_nombre = "ii_admision_unsa";
-       
-    $bd = mysqli_connect($servidor, $usuario, $password, $bd_nombre)
-    or die("No se puede conectar al servidor.");
-?>
